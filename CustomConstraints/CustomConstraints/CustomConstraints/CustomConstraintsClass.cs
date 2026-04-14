bec2e2d6e8447c7741276c5d2cc507c85115447a
codeorbitnova@gmail.com <jaimin.suthar12@gmail.com>
2026-04-14 16:15:29 +0530

    CustomConstraints with country wise listing project

---
+++ CustomConstraints/CustomConstraints/CustomConstraints/CustomConstraintsClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CustomConstraints.CustomConstraints
+{
+    public class CustomConstraintsClass : IRouteConstraint
+    {
+        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if(!values.ContainsKey(routeKey))
+            {
+                return false;
+            }
+
+            Regex regex = new Regex(@"^[0-5]+$");
+            string? value = values[routeKey]?.ToString();
+            if (value != null)
+            {
+                return regex.IsMatch(value);
+            }
+
+            return false;
+        }
+    }
+}
